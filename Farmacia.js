fe088e30baaba050504b15d9561f54950ccabe54
diego-2003 <92651541+diego-2003@users.noreply.github.com>
2022-10-12 10:59:37 -0500

    Update Farmacia.js

--- Farmacia.js
+++ Farmacia.js
@@ -1,4 +1,14 @@
 let text
 let hora = prompt("Hora en la que ingirio su primera medicina del dia")
 
-switch(hora )
+switch(hora) {
+    case hora > 23:
+    document.write("ERROR,horario invalido")
+    break;
+
+    case hora = 23:
+    console.log(hora)
+    document.write("Horario maximo permitido")
+    break;
+
+}
