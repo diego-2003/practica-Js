80152c152bf5544e0c04dfcf54a986ba903f2e9b
diego-2003 <92651541+diego-2003@users.noreply.github.com>
2022-10-26 16:37:52 -0500

    Update js3.js

--- js3.js
+++ js3.js
@@ -1,9 +1,9 @@
+document.write("Informacion de las tomas de medicamento del paciente (n)")
 let horainicial = prompt("Hora en la que ingirio su primera medicina del dia (1-23 hrs):")
 let tomafrecuencia = prompt("Con que frecuencia al dia toma su medicamento? (hasta 12 veces al dia):")
 let n = 1;
 
 for(let i=horainicial; i<=23; i++){
-    let codigo = "<p>La toma " + n + " es a las " + i + " horas</p>";
-    document.write(codigo);
+    document.write("<p><mark><strong>La toma " + n + " es a las " + i + " horas</strong></mark></p>");
     n = n + 1;
 }
