6ca440d93847ae5b79b960558db53740be49fa16
diego-2003 <92651541+diego-2003@users.noreply.github.com>
2022-10-27 18:10:13 -0500

    Update js5.js

--- js5.js
+++ js5.js
@@ -1,1 +1,38 @@
-console.log("Hola")
+//Calculadora
+
+function Proceder(){
+    let numero1 = parseInt(document.getElementById("Val1").value);
+
+    let numero2 = parseInt(document.getElementById("Val2").value);
+
+    let Tipo = document.getElementById("OperacionTipo").value; 
+
+    let valorfinal = document.getElementById("Resultado");
+
+    valorfinal.value = estructura(Tipo, numero1, numero2);
+}
+
+function estructura(operacion, Num1, Num2){
+    let valor = 0;
+    switch (operacion){
+        case "+":
+            valor = Num1 + Num2;
+            break;
+
+        case "-":
+            valor = Num1 - Num2;
+            break;
+
+        case "*":
+            valor = Num1 * Num2;
+            break;
+
+        case "/":
+            valor = Num1 / Num2;
+            break;
+    
+        default:
+            break;
+    }
+    return valor
+}
